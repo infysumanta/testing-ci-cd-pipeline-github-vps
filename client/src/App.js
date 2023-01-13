14a1080b19ecc78bee00e8719dd790cacfc72ea7
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-13 22:05:13 +0530

    another test

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>A Basic Application for Github CI CD</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
